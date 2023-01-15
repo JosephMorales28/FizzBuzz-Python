f447e17e03da42649512372a689652007a2c7676
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-01-16 07:06:37 +0800

    Updated input result and explanation

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -12,3 +12,19 @@ for x in range(1,n,2):
         continue
     else:
         print(x)
+# input:15
+#1
+#Fizz
+#Buzz
+#7
+#Fizz
+#Buzz
+#11
+#13
+
+#explanation
+#all number rounded by 2 will not be printed
+#all number rounded by 3 will be printed by Fizz
+#all number rounded by 5 will be printed by Buzz
+#if number rounded by 3 and 5 will be printed by FizzBuzz
+#the last number input will be break
